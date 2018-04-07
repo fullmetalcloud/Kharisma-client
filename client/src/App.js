8a355e14b6b31a2924dbf2b7ee4bfcfdc677e3df
fullmetalcloud <gueved90@gmail.com>
2018-04-07 13:55:30 -0700

    questions.json added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import logo from './logo.svg';
 import './App.css';
-import question from './questions.json';
-
-
+import questions from './questions.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <p className="App-intro">
+          {console.log(questions)}
+        </p>
       </div>
     );
   }
