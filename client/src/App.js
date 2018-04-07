762634ff4a49659fe9e4c36feeb8b1390962d410
Madeline Wong <mgwong1991@gmail.com>
2018-04-07 15:05:49 -0700

    data driven personality quiz questions and answers

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import questions from './questions.json';
+import questionsList from './questions.json';
 
 class QuestionAnswer extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <h1>
-            This is the question.
-          </h1>
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option1" />
-                  Option 1
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option2" />
-                  Option 2
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option3" />
-                  Option 3
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option4" />
-                  Option 4
-                </label>
+        {questionsList.map(function(questions) {
+          return (
+            <div className="container">
+              <h2>
+                {questions.question}
+              </h2>
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="radio">
+                    <label>
+                      <input type="radio" value="option1" />
+                      {questions.answers[0].answer}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      <input type="radio" value="option2" />
+                      {questions.answers[1].answer}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      <input type="radio" value="option3" />
+                      {questions.answers[2].answer}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      <input type="radio" value="option4" />
+                      {questions.answers[3].answer}
+                    </label>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-class Quiz extends Component {
-  renderQA(i) {
-    return <QuestionAnswer />;
-  }
-
-  render() {
-    return (
-      <div>
-        {this.renderQA(0)};
-        {this.renderQA(1)};
-        {this.renderQA(2)};
-        {this.renderQA(3)};
-        {this.renderQA(4)};
-        {this.renderQA(5)};
-      </div>
-    );
-  }
-}
-
-export default Quiz;
+export default QuestionAnswer;
