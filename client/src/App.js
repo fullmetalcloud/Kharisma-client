f111751894204292afef6229edb8edf4edf6e7a2
Madeline Wong <mgwong1991@gmail.com>
2018-04-07 14:26:27 -0700

    template for personality quiz

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import questions from './questions.json';
 
-class App extends Component {
+class QuestionAnswer extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <h1>
+            This is the question.
+          </h1>
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option1" />
+                  Option 1
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option2" />
+                  Option 2
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option3" />
+                  Option 3
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="option4" />
+                  Option 4
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class Quiz extends Component {
+  renderQA(i) {
+    return <QuestionAnswer />;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderQA(0)};
+        {this.renderQA(1)};
+        {this.renderQA(2)};
+        {this.renderQA(3)};
+        {this.renderQA(4)};
+        {this.renderQA(5)};
+      </div>
+    );
+  }
+}
+
+export default Quiz;
